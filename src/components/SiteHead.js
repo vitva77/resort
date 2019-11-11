7d85e836652573c3a6afce4d4710803f49bdeffb
hanseg <vit.va.77@gmail.com>
2019-11-11 19:14:53 +0300

    commit

---
+++ src/components/SiteHead.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const SiteHead = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Nested Title</title>
+      </Helmet>
+    </>
+  );
+};
+
+export default SiteHead;
